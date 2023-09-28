1977e732949cc664c77e3deaf07c58bd442b62da
Nguyen Van Danh <danhvannguyen000@gmail.com>
2023-09-28 12:56:53 +0700

    first

---
+++ OneDrive/Documents/NetBeansProjects/GeographicManagement/src/Model/EastAsiaCountries.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author danhv
+ */
+public class EastAsiaCountries extends Country {
+    private String countryTerrain;
+
+    public EastAsiaCountries() {
+    }
+
+    public EastAsiaCountries(String countryCode, String countryName, float totalArea, String countryTerrain) {
+        super(countryCode, countryName, totalArea);
+        this.countryTerrain = countryTerrain;
+    }
+
+    public String getCountryTerrain() {
+        return countryTerrain;
+    }
+
+    public void setCountryTerrain(String countryTerrain) {
+        this.countryTerrain = countryTerrain;
+    }
+
+    @Override
+    public void display() {
+        System.out.printf("%-10s%-15s%-15.0f%-10s",countryCode,countryName,totalArea,countryTerrain);
+        System.out.println("");
+    }
+
+        
+    
+    
+}
---
+++ OneDrive/Documents/NetBeansProjects/GeographicManagement/src/View/Menu.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+import Common.Validation;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author danhv
+ */
+
+public abstract class Menu<T> {
+    protected String title;
+    protected ArrayList<T> choice;
+    protected Validation lib;
+
+    public Menu() {
+        choice = new ArrayList<>();
+        lib = new Validation();
+    }
+    
+    public Menu(String title, String[] mchoice) {
+        this.title = title;
+        this.lib = new Validation();
+        choice = new ArrayList<>();
+        for(String s : mchoice){
+            choice.add((T) s);
+        }
+    }
+    //-----------------------------------------------
+    public void display(){
+        System.out.println(title);
+        System.out.println("------------------------------");
+        for (int i=0;i<choice.size();i++){
+            System.out.println((i+1) + ". " + choice.get(i));
+        }
+        System.out.println("------------------------------");
+    }
+    //------------------------------------------------
+    public int getChoice(){
+        display();
+        return lib.getInt("Enter your choice: ", 1, choice.size());
+    }
+    
+    //-------------------------------------------------
+    public abstract void execute(int n);
+    
+    //-------------------------------------------------
+    public void run(){
+        while (true){
+            int n = getChoice();
+            execute(n);
+            if(n>choice.size()){
+                break;
+            }
+        }
+    }
+}
